85c8f6c2b09ecff3b60f8d2c8c760867336249d2
dmccormick521 <dmichaelmccormick@gmail.com>
2022-03-20 17:08:01 -0500

    Game works at a base level (Done) Need to allow only current line edits (Done) Should require full words before submit (Done) Eventually grab a random word for each game Need to only allow alphabet inputs (Done) Could hop between text boxes automatically for convenience? Clean up code later Commented code out better (Done)

--- src/main/java/game.java
+++ src/main/java/game.java
@@ -6,28 +6,36 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+//This is a attempt at making wordle
 public class game implements ActionListener {
+
+    //Lists holds our JTextField objects
+    //Correct tracks current right on each submit
+    //Count tracks our current location in 'Lists'
+    //answer is a placeholder for a random word for now
+
     ArrayList<JTextField> lists;
-    private int count=0;
-    private int correct=0;
+    private int count = 0;
+    private int correct = 0;
     JFrame frame;
     private String answer = "hello";
 
+    //textFieldLimit class is connected here and is what restricts our input correctly for JTextFields
     public game() {
         frame = new JFrame("Wordle or Not");
         JButton submit = new JButton("Submit");
         submit.addActionListener(this);
         lists = new ArrayList<>();
-        for(int x = 0 ; x<20; x++){
+        for (int x = 0; x < 20; x++) {
             lists.add(new JTextField());
             lists.get(x).setDocument(new textFieldLimit(1));
             frame.add(lists.get(x));
-            if(x>4) {
+            if (x > 4) {
                 lists.get(x).setEditable(false);
             }
         }
 
-        GridLayout gl = new GridLayout(5,5);
+        GridLayout gl = new GridLayout(5, 5);
         frame.setLayout(gl);
         frame.add(submit);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -38,55 +46,55 @@ public class game implements ActionListener {
 
     //Need to check for edge cases later
     //Multiple of same character - how to highlight correctly and not overwrite
-    public void actionPerformed(ActionEvent e){
+    public void actionPerformed(ActionEvent e) {
         String s = e.getActionCommand();
-        if(s.equals("Submit")  && correct!=5 && allFilled()){
+        if (s.equals("Submit") && correct != 5 && allFilled()) {
             int i = 0;
-            correct=0;
-            while(i<5){
-                if(answer.charAt(i)==lists.get(count).getText().charAt(0) ){
+            correct = 0;
+            while (i < 5) {
+                if (answer.charAt(i) == lists.get(count).getText().charAt(0)) {
                     lists.get(count).setBackground(Color.green);
                     correct++;
-                }
-                else if(answer.contains(lists.get(count).getText())){
+                } else if (answer.contains(lists.get(count).getText())) {
                     lists.get(count).setBackground(Color.yellow);
-                }
-                else{
+                } else {
                     lists.get(count).setBackground(Color.gray);
                 }
                 lists.get(count).setEditable(false);
                 count++;
                 i++;
             }
-            if(correct!=5) {
+            if (correct != 5) {
                 lineEditable();
             }
 
         }
 
     }
-
-    public void lineEditable(){
-        if(count<20){
-        for(int x = count; x<count+5; x++){
-        lists.get(x).setEditable(true);
+    //Helper method to only allow current line to receive input
+    //Uses count variable to set each JTextField to be editable on current line
+    public void lineEditable() {
+        if (count < 20) {
+            for (int x = count; x < count + 5; x++) {
+                lists.get(x).setEditable(true);
+            }
         }
     }
-    }
-
-    public boolean allFilled(){
-            for(int x = count; x<count+5; x++) {
-                try{
-                    lists.get(x).getText().charAt(0);
-                }
-                catch(Exception e){
-                        return false;
-                }
+    //Checks if current line is filled up
+    //If we hit the 'catch' it's likely because a null pointer and won't take down the program
+    //so it will only run with proper input
+    public boolean allFilled() {
+        for (int x = count; x < count + 5; x++) {
+            try {
+                lists.get(x).getText().charAt(0);
+            } catch (Exception e) {
+                return false;
             }
+        }
         return true;
     }
 
-    public static void main(String[]args){
+    public static void main(String[] args) {
         new game();
     }
 
--- src/main/java/textFieldLimit.java
+++ src/main/java/textFieldLimit.java
@@ -5,6 +5,8 @@ import javax.swing.text.AttributeSet;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.PlainDocument;
 
+//Limits input textfields can receive to a single alphabetic character
+//May decide to stick with only lower case or upper case at some point - or will just convert characters between both
 public class textFieldLimit extends PlainDocument {
     private int limit;
 
