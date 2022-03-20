36822d14128f0f06e7ad967bdf5f123664cbf9da
dmccormick521 <dmichaelmccormick@gmail.com>
2022-03-20 15:13:58 -0500

    Currently Messing with different components and seeing what methods are available

--- src/main/java/game.java
+++ src/main/java/game.java
@@ -2,15 +2,50 @@ package src.main.java;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class game {
-    public static void main(String[] args) {
+import javafx.scene.text.*;
+public class game implements ActionListener {
 
-        JFrame frame = new JFrame("Wordle or Not");
-        JLabel emptyLabel = new JLabel("");
+    private JLabel label;
+    private JFrame frame;
+    private JPanel panel;
+    private JTextField one;
+    public game() {
+        frame = new JFrame("Wordle or Not");
+        JButton submit = new JButton("Submit");
+        submit.addActionListener(this);
+        //Text t = new Text(10, 50, "This is a test");
+
+
+        one = new JTextField(0);
+        JTextField Input = new JTextField(0);
+
+        // submit.addActionListener(t);
+        frame.getContentPane().add(Input, BorderLayout.NORTH);
+        frame.getContentPane().add(submit, BorderLayout.SOUTH);
+        frame.getContentPane().add(one, BorderLayout.CENTER);
+        one.setBounds(50, 200, 50, 50);
+
+        //field.addActionListener(this);
+        //frame.getContentPane().add(one, new Dimension(50, 50, 50, 50));
+
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
-        frame.pack();
+        frame.setSize(500, 500);
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
+    public void actionPerformed(ActionEvent e){
+        String s = e.getActionCommand();
+        if(s.equals("Submit")){
+            one.setBackground(Color.yellow);
+        }
+    }
+
+    public static void main(String[]args){
+        new game();
+    }
+
 }
