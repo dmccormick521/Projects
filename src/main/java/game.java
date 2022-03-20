b3b67ae27f1990e4b2874323e137d6ea65991ebc
dmccormick521 <dmichaelmccormick@gmail.com>
2022-03-20 16:24:16 -0500

    Game works at a base level. Need to allow only current line edits Should require full words before submit Eventually grab a random word for each game Need to only allow alphabet inputs Could hop between text boxes automatically for convenience?

--- src/main/java/game.java
+++ src/main/java/game.java
@@ -4,45 +4,59 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javafx.scene.text.*;
 public class game implements ActionListener {
-
-    private JLabel label;
+    ArrayList<JTextField> lists = new ArrayList<JTextField>();
+    private int count=0;
     private JFrame frame;
-    private JPanel panel;
-    private JTextField one;
+    private String answer = "hello";
+
     public game() {
         frame = new JFrame("Wordle or Not");
         JButton submit = new JButton("Submit");
         submit.addActionListener(this);
-        //Text t = new Text(10, 50, "This is a test");
+        lists = new ArrayList<JTextField>();
+        for(int x = 0 ; x<20; x++){
+            lists.add(new JTextField());
+            frame.add(lists.get(x));
+        }
 
-
-        one = new JTextField(0);
         JTextField Input = new JTextField(0);
-
-        // submit.addActionListener(t);
-        frame.getContentPane().add(Input, BorderLayout.NORTH);
-        frame.getContentPane().add(submit, BorderLayout.SOUTH);
-        frame.getContentPane().add(one, BorderLayout.CENTER);
-        one.setBounds(50, 200, 50, 50);
-
-        //field.addActionListener(this);
-        //frame.getContentPane().add(one, new Dimension(50, 50, 50, 50));
-
-
+        GridLayout gl = new GridLayout(5,5);
+        frame.setLayout(gl);
+        frame.add(submit);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500, 500);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
+
+    //Need to check for edge cases later
+    //Multiple of same character - how to highlight correctly and not overwrite
     public void actionPerformed(ActionEvent e){
         String s = e.getActionCommand();
         if(s.equals("Submit")){
-            one.setBackground(Color.yellow);
+            int i = 0;
+            while(i<5){
+                if(answer.charAt(i)==lists.get(count).getText().charAt(0) ){
+                    lists.get(count).setBackground(Color.green);
+                }
+                else if(answer.contains(lists.get(count).getText())){
+                    lists.get(count).setBackground(Color.yellow);
+                }
+                else{
+                    lists.get(count).setBackground(Color.gray);
+                }
+                lists.get(count).setEditable(false);
+                count++;
+                i++;
+            }
         }
+
     }
+
 
     public static void main(String[]args){
         new game();
