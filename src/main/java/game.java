3200289b28148616fa0e9d5029a8d85bd6e2a770
dmccormick521 <dmichaelmccormick@gmail.com>
2022-04-12 14:15:14 -0500

    Game works at a base level (Done) Need to allow only current line edits (Done) Should require full words before submit (Done) Eventually grab a random word for each game Need to only allow alphabet inputs (Done) Could hop between text boxes automatically for convenience? Clean up code later Commented code out better (Done) Increased Font Size(Done)

--- src/main/java/game.java
+++ src/main/java/game.java
@@ -4,46 +4,32 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.RandomAccessFile;
 import java.util.ArrayList;
 
-//This is a attempt at making wordle
+//This is an attempt at making wordle
 public class game implements ActionListener {
 
-    //Lists holds our JTextField objects
+    //List holds our JTextField objects
     //Correct tracks current right on each submit
     //Count tracks our current location in 'Lists'
     //answer is a placeholder for a random word for now
-
+    File fileInput = new File("src/main/java/answers");
     ArrayList<JTextField> lists;
     Font font1 = new Font("SansSerif", Font.BOLD, 30);
     private int count = 0;
     private int correct = 0;
     JFrame frame;
-    private String answer = "hello";
+    private final String answer = gameAnswer(fileInput);
 
     //textFieldLimit class is connected here and is what restricts our input correctly for JTextFields
-    public game() {
+    public game() throws Exception {
         frame = new JFrame("Wordle or Not");
         JButton submit = new JButton("Submit");
         submit.addActionListener(this);
-        lists = new ArrayList<>();
-        for (int x = 0; x < 20; x++) {
-            lists.add(new JTextField());
-            lists.get(x).setDocument(new textFieldLimit(1));
-            lists.get(x).setFont(font1);
-            frame.add(lists.get(x));
-            if (x > 4) {
-                lists.get(x).setEditable(false);
-            }
-        }
-
-        GridLayout gl = new GridLayout(5, 5);
-        frame.setLayout(gl);
-        frame.add(submit);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 500);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+        initLists();
+        initGridAndFrame(submit);
     }
 
     //Need to check for edge cases later
@@ -84,7 +70,7 @@ public class game implements ActionListener {
     }
     //Checks if current line is filled up
     //If we hit the 'catch' it's likely because a null pointer and won't take down the program
-    //so it will only run with proper input
+    //,so it will only run with proper input
     public boolean allFilled() {
         for (int x = count; x < count + 5; x++) {
             try {
@@ -96,7 +82,43 @@ public class game implements ActionListener {
         return true;
     }
 
-    public static void main(String[] args) {
+    public void initLists() {
+        lists = new ArrayList<>();
+        for (int x = 0; x < 20; x++) {
+            lists.add(new JTextField());
+            lists.get(x).setDocument(new textFieldLimit(1));
+            lists.get(x).setFont(font1);
+            frame.add(lists.get(x));
+            if (x > 4) {
+                lists.get(x).setEditable(false);
+            }
+        }
+    }
+
+    public void initGridAndFrame(JButton submit) {
+        GridLayout gl = new GridLayout(5, 5);
+        frame.setLayout(gl);
+        frame.add(submit);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    public String gameAnswer(File file) throws Exception {
+        RandomAccessFile f = new RandomAccessFile(file, "r");
+        long random = System.currentTimeMillis();
+        while (random > f.length()) {
+            random %= 100;
+        }
+        f.seek(random);
+        f.readLine();
+        String randomString = f.readLine();
+        f.close();
+        return randomString;
+    }
+
+    public static void main(String[] args) throws Exception {
         new game();
     }
 
