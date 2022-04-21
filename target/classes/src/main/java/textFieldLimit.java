4831200aa21ab669c541eccfc828b10107812684
dmccormick521 <dmichaelmccormick@gmail.com>
2022-04-21 15:16:59 -0500

    Refactored JFrame rows into objects Cleaned up code Provided more comment clarity Fixed package issue Merged

---
+++ target/classes/src/main/java/textFieldLimit.java
@@ -0,0 +1,25 @@
+package src.main.java;
+
+
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.PlainDocument;
+
+//Limits input textfields can receive to a single alphabetic character
+//May decide to stick with only lower case or upper case at some point - or will just convert characters between both
+public class textFieldLimit extends PlainDocument {
+    private final int limit;
+
+    public textFieldLimit(int limit) {
+        super();
+        this.limit = limit;
+    }
+
+    public void insertString( int offset, String  str, AttributeSet attr ) throws BadLocationException {
+        if (str == null) return;
+
+        if (((getLength() + str.length()) <= limit) && str.matches("[a-z]")) {
+            super.insertString(offset, str, attr);
+        }
+    }
+}
